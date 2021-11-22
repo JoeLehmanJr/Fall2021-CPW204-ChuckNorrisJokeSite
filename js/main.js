769b5b8c7dc86b326af249676a0c732445979216
JoeLehmanJr <joejr65@live.com>
2021-11-22 12:37:17 -0800

    Finished basic design.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,44 @@
+var SingleJoke = (function () {
+    function SingleJoke() {
+    }
+    return SingleJoke;
+}());
 window.onload = function () {
+    var jokeButton = document.getElementById('get-joke');
+    jokeButton.onclick = main;
 };
+function main() {
+    var http = new XMLHttpRequest();
+    http.open("GET", "https://api.icndb.com/jokes/random?limitTo=[nerdy]");
+    http.onreadystatechange = processRequest;
+    http.send();
+}
+function processRequest() {
+    var http = this;
+    if (http.readyState == 4 && http.status == 200) {
+        var response = JSON.parse(http.responseText).value;
+        console.log("Printing response frow web API...");
+        console.log(response);
+        console.log("Printing the joke ID...");
+        console.log(response.id);
+        console.log("Printing the joke");
+        console.log(response.joke);
+        console.log("Printing the joke category");
+        console.log(response.categories);
+        displayJoke(response);
+    }
+}
+function displayJoke(j) {
+    var displayDiv = document.getElementById("display-joke");
+    var jokeIdSpan = displayDiv.querySelector("h2 > span");
+    jokeIdSpan.innerText = j.id.toString();
+    var jokeParagraph = displayDiv.querySelector("p");
+    jokeParagraph.innerHTML = j.joke;
+    var catList = displayDiv.querySelector("ul");
+    catList.innerHTML = "";
+    for (var i = 0; i < j.categories.length; i++) {
+        var nextCat = document.createElement("li");
+        nextCat.innerText = j.categories[i];
+        catList.appendChild(nextCat);
+    }
+}
--- ts/main.ts
+++ ts/main.ts
@@ -1,3 +1,75 @@
+class SingleJoke{
+    /**
+     * The joke's identifier.
+     */
+    id: number;
+
+    /**
+     * The text of the joke.
+     */
+    joke: string;
+
+    /**
+     * The category of the joke. eg. "explicit", "nerdy"
+     */
+    categories: string[];
+}
+
 window.onload = function() {
-    
+    let jokeButton = document.getElementById('get-joke');
+    jokeButton.onclick = main;
+}
+
+function main() {
+    let http = new XMLHttpRequest();
+    // Prepare the request to the server and
+    // GET request asks the server for data to be sent
+    // The URL is the website we are requesting data from
+    http.open("GET","https://api.icndb.com/jokes/random?limitTo=[nerdy]");
+
+    // Function to handle different readyStates
+    http.onreadystatechange = processRequest;
+
+    // Sends the request to the server
+    http.send();
+}
+
+function processRequest() {
+    let http = <XMLHttpRequest>this;
+    if(http.readyState == 4 && http.status == 200){
+        // alert("Check the console because we are finished!");
+        let response:SingleJoke = JSON.parse(http.responseText).value;
+
+        console.log("Printing response frow web API...");
+        console.log(response);
+        console.log("Printing the joke ID...");
+        console.log(response.id);
+        console.log("Printing the joke");
+        console.log(response.joke);
+        console.log("Printing the joke category");
+        console.log(response.categories);
+
+        displayJoke(response);
+    }
+}
+
+function displayJoke(j:SingleJoke){
+    let displayDiv = document.getElementById("display-joke");
+
+    // Display joke ID
+    let jokeIdSpan = <HTMLElement>displayDiv.querySelector("h2 > span");
+    jokeIdSpan.innerText = j.id.toString();
+
+    // Display joke text
+    let jokeParagraph = displayDiv.querySelector("p");
+    jokeParagraph.innerHTML = j.joke;
+
+    // Display joke categories
+    let catList = displayDiv.querySelector("ul");
+    catList.innerHTML = "";
+    for (let i = 0; i < j.categories.length; i++){
+         let nextCat = document.createElement("li");
+         nextCat.innerText = j.categories[i];
+         catList.appendChild(nextCat);
+    }
 }
